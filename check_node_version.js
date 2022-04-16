90874651e283e391933b0d527e1c22f3ab399d24
Matt Thommes <matt@thomm.es>
2022-04-16 10:47:35 -0500

    Check node version output change

--- check_node_version.js
+++ check_node_version.js
@@ -2,5 +2,5 @@ const semver = require ('semver');
 const { engines } = require ('./package');
 const version = engines.node;
  if (!semver.satisfies(process.version, version)) {
-  throw new Error(`The current node version ${process.version} does not satisfy the required version ${version}`);
+  throw new Error(`The current node version ${process.version} does not satisfy the required version ${version}. Run 'nvm use' to use use the correct version.`);
 }
