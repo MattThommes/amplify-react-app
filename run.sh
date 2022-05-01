47b5256e8250b23bec40a4ae2af466c7c2400f73
Matt Thommes <matt@thomm.es>
2022-05-01 11:39:36 -0500

    Building out bash script

--- run.sh
+++ run.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo "-> Initializing git..."
+echo "  -> Removing any existing git setup..."
+rm -rf .git
+
+echo "-> Cloning Amplify React App repo..."
+git clone git@github.com:MattThommes/amplify-react-app.git . --quiet
+clone_success=$?
+if [ clone_success <> 0 ] ; then
+	echo -e "${RED}Failed to clone the repo (error code $clone_success)${NC}"
+	exit 1
+fi
+
 echo "Please provide your repo SSH clone URI for your project:"
-
 read project_repo_uri
+echo "-> Configuring git remotes..."
+git remote remove upstream
+git remote add upstream $project_repo_uri
+git remote remove origin
 
-git remote add upstream project_repo_uri
-
-echo "-> Updating README..."
+date=$(date +%m/%d/%Y)
+echo "-> Updating readme..."
 > README.md
-echo "# Test" > README.md
+echo "# Custom Amplify React App - $date" > README.md
 
+# source nvm so it is found
+. ~/.nvm/nvm.sh
 echo "-> Setting correct Node version..."
 nvm use
 
 echo "-> Checking Amplify version..."
 amplify_version=$(amplify --version)
 
-echo "->> Amplify version found: ${amplify_version}"
+echo "  -> Amplify version found: ${amplify_version}"
+
+if [ "$amplify_version" <> "7.6.26" ] ; then
+	echo -e "${RED}Amplify version incorrect${NC}"
+fi
