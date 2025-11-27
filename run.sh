ac7e99cb4a2687f819820b05f7990143e693ab67
Matt Thommes <matt@thomm.es>
2025-11-27 11:07:51 -0600

    Set commands for run

--- run.sh
+++ run.sh
@@ -2,3 +2,11 @@
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
+
+echo "-> Setting correct Node version..."
+source "$(brew --prefix nvm)/nvm.sh"
+nvm use
+nvm list
+
+echo "-> Running local server (press Ctrl+C to stop)..."
+npm run dev
