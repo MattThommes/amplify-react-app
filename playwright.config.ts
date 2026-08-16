569a08f798df79a4ef4be77781ace7326857b070
Matt Thommes <matt@thomm.es>
2026-08-16 17:21:27 -0500

    chore: Use list reporter in CI for verbose test names in logs

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'html',
+  reporter: process.env.CI ? [['list'], ['github'], ['html', { open: 'never' }]] : 'html',
   use: {
     baseURL: baseURL,
     trace: 'on-first-retry',
